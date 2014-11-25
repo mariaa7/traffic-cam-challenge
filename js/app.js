7c304a47f4daaad063a8ad59f7f0b60377891e97
mariaa7 <mariaa7@uw.edu>
2014-11-24 21:48:31 -0800

    Modified index HTML and app JS

--- js/app.js
+++ js/app.js
@@ -3,11 +3,74 @@
 
 "use strict";
 
-//put your code here to create the map, fetch the list of traffic cameras
-//and add them as markers on the map
-//when a user clicks on a marker, you should pan the map so that the marker
-//is at the center, and open an InfoWindow that displays the latest camera
-//image
-//you should also write the code to filter the set of markers when the user
-//types a search phrase into the search box
+var markers = [];
+var map;
+
+$(document).ready(function() {
+
+	var mapOptions = {
+		center: {lat: 47.6, lng: -122.3},
+		zoom: 12
+	}
+
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	var infoWindow = new google.maps.InfoWindow();
+
+	//Adds markers to map
+	$.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+		.done(function(data) {
+			console.log('success!');
+			$.each(data, function(i, item) {
+				var latLng = new google.maps.LatLng(item.location.latitude, item.location.longitude);	
+				var marker = new google.maps.Marker({
+	    			position: latLng,
+	    			map: map,
+	    			label: item.cameralabel,
+	    			imgSrc: item.imageurl.url
+				});
+				markers.push(marker);
+				google.maps.event.addListener(marker, 'click', markerClick)
+			});
+		})
+		.fail(function(error) {
+			alert('Your request has failed');
+		})
+		.always(function() {
+		
+		});
+
+	//Pans to marker and opens infowindow when marker is clicked
+	function markerClick() {
+		map.panTo(this.getPosition());
+		var contentString = '<p>' + this.label + '</p><img src=' + 
+		this.imgSrc + '>';
+		infoWindow.setContent(contentString);
+		infoWindow.open(map, this);
+	}
+
+	
+
+});
+
+//Filters markers shown on map by search input
+$('#search').bind("search keyup", function() {
+		var searchTerm = document.getElementById('search').value.toLowerCase();
+		$.each(markers, function(index, value) {
+			var markerLabel = value.label.toLowerCase();
+			if (markerLabel.indexOf(searchTerm) == -1) {
+				value.setMap(null);
+			} else {
+				value.setMap(map);
+			}
+		});
+});
+
+
+
+
+
+
+
+
 
